6e75d63a54236b2f250370654db03d0ce7d6e46f
sanyog12 <94354794+sanyog12@users.noreply.github.com>
2023-03-20 20:33:37 +0530

    Add files via upload

---
+++ searchingInRotatedSortedArray.cpp
@@ -0,0 +1,110 @@
+https://www.codingninjas.com/codestudio/problems/search-in-rotated-sorted-array_1082554?leftPanelTab=1
+
+
+
+
+
+
+
+
+int getPivot(vector<int>& arr, int n){
+
+    int s = 0;
+
+    int e = n-1;
+    
+    int mid = s+(e-s)/2;
+
+    while(s<e){
+
+        if(arr[0] <= arr[mid]){
+
+            s = mid + 1;
+
+        }
+
+
+        else{
+
+            e = mid ;
+
+        }
+
+        mid = s+(e-s)/2;
+
+    }
+
+    return s;
+
+}
+
+
+int binarySearch(vector<int>& arr,int s, int e, int k){
+    int start = s;
+    int end = e;
+    int mid = start + (end-start)/2;
+
+    while(start<=end){
+        
+        if(arr[mid] == k){
+
+            return mid;
+            
+        }
+
+        if(k > arr[mid]){
+
+            start = mid + 1;
+
+        }
+
+        else{
+
+            end = mid - 1;
+
+        }
+
+        mid = start + (end-start)/2;
+    }
+
+    return -1;
+
+}
+
+
+int findPosition(vector<int>& arr, int n, int k){
+
+    int pivot = getPivot(arr,n);
+
+    if(k >= arr[pivot] && k <= arr[n-1]){
+
+        return binarySearch(arr, pivot, n-1, k);
+
+    }
+
+    else{
+
+        return binarySearch(arr, 0, pivot - 1, k);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
